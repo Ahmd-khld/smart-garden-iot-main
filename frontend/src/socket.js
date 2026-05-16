c7d94fe47dfc660dfd2336a40663bf161fd1dc0f
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-05-17 01:43:40 +0300

    2

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -8,4 +8,6 @@ export const socket = io('http://localhost:5000', {
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
+  transports: ['websocket'],
+  withCredentials: true
 });
